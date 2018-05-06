3775d04b7609c321c06f4cc973a01458e892ada3
Lingheng Meng <linghengmeng@yahoo.com>
2018-05-05 20:02:43 -0400

    t

--- LAS_Env_interface_overall_design.py
+++ LAS_Env_interface_overall_design.py
@@ -5,10 +5,11 @@ Created on Sat May  5 16:07:35 2018
 
 @author: jack.lingheng.meng
 
-This is a overall framework of non-distributed simultion environment.
-
 """
 
+"""
+This is an overall framework of non-distributed simultion environment.
+"""
 class LivingArchitectureEnv():
     def __init__(self):
         """
@@ -90,3 +91,113 @@ class LivingArchitectureEnv():
         start_simulation()
         self._observation = self._self_observe()
         return self._observation
+    
+"""
+This is an overall design of agent
+"""
+class Agent():
+    def __init__(self):
+        """
+        initialize
+        """
+        # Hyperparameters
+        
+        # Variables about single experience
+        self._observationOld = []   # observation at time t
+        self._observationNew = []   # observation at time t+1
+        self._actionOld = []        # action at time t
+        self._actionNew = []        # action at time t+1
+        # Memory
+        self._memory = []            # hard memory storing every experience
+        # Components in an agent
+        #   Component: actor-critic model i.e. policy and value function
+        self._actorModel = self._create_actor_model()
+        self._criticModel = self._create_critic_modle()
+        #   Component: environment model
+        self._environmentModel = self._create_environment_model()
+        #   Component: intrinsic motivation model
+        self._intrinsicMotivationModel = self._create_intrinsic_motivation_model()
+        
+    # ========================================================================= #
+    #                       Components or Models Definitions                      #
+    # ========================================================================= #    
+    def _create_actor_model(self):
+        """
+        Create actor model:
+            action = actorModel.predict(observation)
+        """
+        
+    def _create_critic_model(self):
+        """
+        Create critic model:
+            QValue = criticModel.predict(observation, action)
+        """
+        
+    def _create_environment_model(self):
+        """
+        Create environment model:
+            obervationNew, reward = environmentModel.predict(observatin, action)
+        """
+        
+    def _create_intrinsic_motivation_model(self):
+        """
+        Create intrinsic motivation model:
+            intrinsicMotivationModel.predict()
+        """
+        
+    # ========================================================================= #
+    #                      perceive and remember experiences                    #
+    # ========================================================================= #        
+    def perceive(self, observation, reward, done = False, info = []):
+        """
+        Perceive observation and reward from environment after an interaction
+        Input: observation, reward, done, info
+        """
+        self._observationNew = observation
+        self._reward = reward
+        self._done = done
+        self._remember(self._observationOld, self._actionOld, self._observation_new, self._reward)
+        
+    def _remember(self, observationOld, action_Old, observationNew, reward):
+        """
+        Store (observationOld, action_Old, observationNew, reward)
+        """
+        self._memory.append([observationOld, action_Old, observationNew, reward])
+        #self._
+    # ========================================================================= #
+    #                               Train Agent                                 #
+    # ========================================================================= #        
+    def _train(self):
+        """
+        Train agent
+        """
+        samples = random.sample(self._memory, batch_size)   # prepare batch
+        # train actor-critic
+        self._train_critic(samples)
+        self._train_actor(samples)
+        # train environment model
+        self._train_environment_model(samples)
+        
+    def _train_critic(self, samples):
+        """
+        Train critic
+        """
+        
+    def _train_actor(self, samples):
+        """
+        Train actor
+        """
+        
+    def _train_environment_model(self, samples):
+        """
+        Train environment model
+        """
+    def _train_intrinsic_motivation_model(self, samples):
+        """
+        Train intrinsic motivation model
+        """
+    # ========================================================================= #
+    #                              Decide New Action                            #
+    # ========================================================================= #        
+    def act(self):
+        
